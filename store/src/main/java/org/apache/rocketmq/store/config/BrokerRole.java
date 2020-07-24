1ea0353378a615b07e3823e18ca62785810d3126
dongwei8 <dongwei8@meicloud.com>
2020-07-24 16:35:26 +0800

    添加注释信息

--- store/src/main/java/org/apache/rocketmq/store/config/BrokerRole.java
+++ store/src/main/java/org/apache/rocketmq/store/config/BrokerRole.java
@@ -17,7 +17,7 @@
 package org.apache.rocketmq.store.config;
 
 public enum BrokerRole {
-    ASYNC_MASTER,
-    SYNC_MASTER,
-    SLAVE;
+    ASYNC_MASTER, // 异步模式下面的master节点
+    SYNC_MASTER, // 同步模式下面的master节点
+    SLAVE;      // 从节点
 }
